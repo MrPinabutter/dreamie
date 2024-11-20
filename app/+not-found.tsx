b52b7d663b46d663c435c7d85cdae0de58b55c2b
MrPinabutter <vitorafael.r.c@gmail.com>
2024-11-19 23:44:32 -0300

    ✨ Update not found screen

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,30 +1,19 @@
 import { Link, Stack } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
-
+import { Text, View } from "react-native";
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: "Oops!" }} />
-      <View style={styles.container}>
-        <Text>This screen doesn't exist.</Text>
-        <Link href="" style={styles.link}>
+
+      <View className="flex-1 items-center justify-center p-5">
+        <Text className="font-bold text-2xl">Erro 404</Text>
+        <Text>Tela não encontrada!</Text>
+
+        <Link href="/(tabs)" className="mt-4 py-4">
           <Text>Go to home screen!</Text>
         </Link>
       </View>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
